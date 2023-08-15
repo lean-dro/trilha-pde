4f306e211399ba936325b67fe1e9d6854b2ce6b6
lean-dro <leandro.saraiva@sptech.school>
2023-08-15 14:30:41 -0300

    Primeira classe + Atributos + Propriedades

---
+++ SegundoProjetoPOO/ScreenSound/Musica.cs
@@ -0,0 +1,24 @@
+﻿class Musica{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool Disponivel { get; set; }
+
+   
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {nome}");
+        if (Disponivel)
+        {
+            Console.WriteLine($"Disponível no Plano");
+        }
+        else
+        {
+            Console.WriteLine($"Adquira o Plano Plus");
+        }
+    }
+}
+
---
+++ SegundoProjetoPOO/ScreenSound/Program.cs
@@ -0,0 +1,17 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Levanta e Anda";
+musica1.artista = "Emicida";
+musica1.duracao = 400;
+musica1.Disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Até Que Durou";
+musica2.artista = "Péricles";
+musica2.duracao = 500;
+musica2.Disponivel= false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+
+
+

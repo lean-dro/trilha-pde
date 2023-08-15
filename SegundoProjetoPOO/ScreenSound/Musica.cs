78ec6cca7f9299b9ede9126d24a7ffda19932e0f
lean-dro <leandro.saraiva@sptech.school>
2023-08-15 14:47:26 -0300

    Alteração de propriedade

--- SegundoProjetoPOO/ScreenSound/Musica.cs
+++ SegundoProjetoPOO/ScreenSound/Musica.cs
@@ -1,16 +1,22 @@
 ﻿class Musica{
-    public string nome;
-    public string artista;
-    public int duracao;
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
     public bool Disponivel { get; set; }
+    public string DescricaoResumida
+    {
+        get
+        {
+            return $"{Nome} - {Artista}";
+        }
+    }
 
-   
 
     public void ExibirFichaTecnica()
     {
-        Console.WriteLine($"Nome: {nome}");
-        Console.WriteLine($"Artista: {artista}");
-        Console.WriteLine($"Duração: {nome}");
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Nome}");
         if (Disponivel)
         {
             Console.WriteLine($"Disponível no Plano");
--- SegundoProjetoPOO/ScreenSound/Program.cs
+++ SegundoProjetoPOO/ScreenSound/Program.cs
@@ -1,13 +1,13 @@
 ﻿Musica musica1 = new Musica();
-musica1.nome = "Levanta e Anda";
-musica1.artista = "Emicida";
-musica1.duracao = 400;
+musica1.Nome = "Levanta e Anda";
+musica1.Artista = "Emicida";
+musica1.Duracao = 400;
 musica1.Disponivel = true;
-
+Console.WriteLine(musica1.DescricaoResumida);
 Musica musica2 = new Musica();
-musica2.nome = "Até Que Durou";
-musica2.artista = "Péricles";
-musica2.duracao = 500;
+musica2.Nome = "Até Que Durou";
+musica2.Artista = "Péricles";
+musica2.Duracao = 500;
 musica2.Disponivel= false;
 
 musica1.ExibirFichaTecnica();
